d3e565232cd35eb5990912e20a7f7ef78b48b65b
Isabela Marques <88113608+isamarques@users.noreply.github.com>
2022-09-17 22:39:51 -0300

    Update Principal.java

--- view/Principal.java
+++ view/Principal.java
@@ -10,6 +10,10 @@ public class Principal {
 		ConverteController converte = new ConverteController();
 		
 			int valor = Integer.parseInt(JOptionPane.showInputDialog("Insira um número: "));
+		
+			if (valor > 1000) {
+				System.out.println("Número inválido. Insira novamente.");
+			}
 			
 			String resto = converte.decToBin(valor, pilha);
 			System.out.println("==================");
