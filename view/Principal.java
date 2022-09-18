b3b852c4cb0b32ff6b5373787055b7f4ae64cea5
Isabela Marques <88113608+isamarques@users.noreply.github.com>
2022-09-17 22:35:01 -0300

    Update Principal.java

--- view/Principal.java
+++ view/Principal.java
@@ -3,17 +3,16 @@ import controller.ConverteController;
 import model.PilhaInt;
 import javax.swing.*;
 
-
 public class Principal {
 
 	public static void main(String[] args) throws Exception {
 		PilhaInt pilha = new PilhaInt();
 		ConverteController converte = new ConverteController();
 		
-			int valor = Integer.parseInt(JOptionPane.showInputDialog("Insira um número: "));
+			int valor = Integer.parseInt(JOptionPane.showInputDialog("Insira um nÃºmero: "));
 			
 			String resto = converte.decToBin(valor, pilha);
 			System.out.println("==================");
-			System.out.println("O binário de " +valor+ " é " +resto);
+			System.out.println("O binÃ¡rio de " +valor+ " Ã© " +resto);
 	}
 }
